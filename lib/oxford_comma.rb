60eac211d2a10eebc8294c94d14de29e17e4b09f
Joseph Perez <hello@josephdanielperez.com>
2020-06-28 00:58:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   else
-    array.last = "and " + array.last
+    array[-1] = "and " + array[-1]
     list.join(", ")
   end
 end
